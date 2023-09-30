87dba4ebcab0aab5f0d3be27a0c9c8ecedabb8bd
Алексей <96423703+AlexFerji@users.noreply.github.com>
2023-09-30 17:48:25 +0300

    Update views.py

--- robots/views.py
+++ robots/views.py
@@ -5,8 +5,6 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
 from django.views import View
 
-
-from .validators import validate_robot_data
 from .models import Robot
 
 
